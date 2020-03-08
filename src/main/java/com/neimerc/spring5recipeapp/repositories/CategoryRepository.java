849aff67e6c1d8bba6b0d708dec954ba6a0df6d2
Neimerc <Neil_Mercury@yahoo.es>
2020-03-08 17:39:20 +0100

    Added Spring Data JPA Query Methods

--- src/main/java/com/neimerc/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/neimerc/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.neimerc.spring5recipeapp.repositories;
 import com.neimerc.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
