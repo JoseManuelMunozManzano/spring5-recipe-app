3d3743565390d8a6fcbe6a5f100c08eea735eab8
Neimerc <Neil_Mercury@yahoo.es>
2020-04-03 22:38:50 +0200

    Adding IndexControllerTest

---
+++ src/test/java/com/neimerc/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,38 @@
+package com.neimerc.spring5recipeapp.controllers;
+
+import com.neimerc.spring5recipeapp.service.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+
+        assertEquals(indexController.getIndexPage(model), "index");
+
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
