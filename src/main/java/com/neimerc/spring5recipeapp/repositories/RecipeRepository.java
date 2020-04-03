ca84fb7536ca7c96f1fdf1b0ddbc43fb71c65764
Neimerc <Neil_Mercury@yahoo.es>
2020-04-03 20:08:30 +0200

    Adding Mock example

--- src/main/java/com/neimerc/spring5recipeapp/repositories/RecipeRepository.java
+++ src/main/java/com/neimerc/spring5recipeapp/repositories/RecipeRepository.java
@@ -3,10 +3,10 @@ package com.neimerc.spring5recipeapp.repositories;
 import com.neimerc.spring5recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+import java.util.Set;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
-    List<Recipe> findAll();
+    Set<Recipe> findAll();
 
 }
