86370a75b1832469d8d840c565cefd3e6f3e1355
Neimerc <Neil_Mercury@yahoo.es>
2020-04-10 08:07:53 +0200

    Adding create new recipe form and controller methods

--- src/main/java/com/neimerc/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/neimerc/spring5recipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.neimerc.spring5recipeapp.controllers;
 
+import com.neimerc.spring5recipeapp.commands.RecipeCommand;
 import com.neimerc.spring5recipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,6 +23,21 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "/recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }
